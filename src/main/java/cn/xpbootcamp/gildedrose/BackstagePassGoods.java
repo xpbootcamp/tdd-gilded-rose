ef3fc9784ca13b3bc5089fb372bfef780e94ab74
Yanyan Huang <yyhuang@thoughworks.com>
2020-05-18 12:38:33 +0800

    refactor magic number

--- src/main/java/cn/xpbootcamp/gildedrose/BackstagePassGoods.java
+++ src/main/java/cn/xpbootcamp/gildedrose/BackstagePassGoods.java
@@ -18,15 +18,19 @@ public class BackstagePassGoods {
     }
 
     public void passDays(int pastDays) {
+        int GOODS_MAX_VALUE = 50;
+        int GOODS_MIN_VALUE = 0;
+
         boolean is_expire_in_ten_days = this.sellIn - pastDays < 10;
         boolean is_expire_in_five_days = this.sellIn - pastDays < 5;
         boolean is_expire = this.sellIn - pastDays < 0;
+
         int DAILY_ADD_TWO_POINT = 2;
         int DAILY_ADD_THREE_POINT = 3;
 
         this.sellIn--;
         if (is_expire) {
-            this.quantity = 0;
+            this.quantity = GOODS_MIN_VALUE;
         } else if (is_expire_in_five_days) {
             this.quantity += DAILY_ADD_THREE_POINT;
         } else if (is_expire_in_ten_days) {
@@ -35,6 +39,6 @@ public class BackstagePassGoods {
             this.quantity++;
         }
 
-        this.quantity = Math.min(50, this.quantity);
+        this.quantity = Math.min(GOODS_MAX_VALUE, this.quantity);
     }
 }
